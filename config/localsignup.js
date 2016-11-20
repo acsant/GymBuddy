191ad4d8a169df40f778ec3a6f6a9bb897610930
acsant <acsant@uwaterloo.ca>
2016-11-20 03:35:33 -0500

    minor changes

--- config/localsignup.js
+++ config/localsignup.js
@@ -22,10 +22,10 @@ module.exports = function (passport) {
         var userGoals = req.body.objectives;
         // Create new user instance on signup
         newUser.local.auth.email = email;
-        newUser.local.email = email;
         newUser.local.first_name = req.body.first_name;
         newUser.local.last_name = req.body.last_name;
         newUser.local.phone_num = req.body.phone_num;
+        newUser.local.weight = req.body.weight;
         newUser.local.auth.password = newUser.generateHash(password);
         newUser.local.age = req.body.age;
         newUser.local.body_fat = req.body.body_fat;
